1b40043daa8d62aca7a17251db02a004b80f6316
hassaan02 <136227511+hassaan02@users.noreply.github.com>
2023-07-29 20:57:46 +0500

    initial commit

---
+++ arithmetic_operators.dart
@@ -0,0 +1,41 @@
+void main(){
+
+// Addition operator
+num num1 = 15;
+num num2 = 30;
+num result = num1 + num2;
+print(result);
+
+// Subtraction Operator
+num numA = 18;
+num numB = 12;
+num result_2 = numA - numB;
+print(result_2);
+
+// Multiply operator
+num numX = 33;
+num numY = 2;
+num result_3 = numX * numY;
+print(result_3);
+
+// Divide operator
+num numK = 45;
+num numM = 2;
+num result_4 = numK / numM;
+print(result_4);
+
+// Roundoff divide operator
+num numP = 33;
+num numH = 7;
+num result_5 = numP ~/ numH;
+print(result_5);
+
+// Remainder operator
+num numR = 32;
+num numT = 3;
+num result_6 = numR % numT;
+print(result_6);
+
+
+
+}
